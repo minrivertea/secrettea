55f247e35186c98b638d14afda5026e1b6f1a483
minrivertea <chris@minrivertea.com>
2012-12-23 22:57:23 +0800

    lots of changes

---
+++ website/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from datetime import date
+
+
+class Tour(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    promo_image = models.ImageField(upload_to='tours', blank=True, null=True)
+    intro_content = models.TextField(blank=True, null=True)
+    main_content = models.TextField(blank=True, null=True)
+    remaining_places = models.IntegerField(default=0)
+    price = models.IntegerField(blank=True, null=True)
+    starting_point = models.CharField(max_length=200, blank=True, null=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    
+    def __unicode__(self):
+        month = self.end_date.strftime("%B")
+        name = "%s (%s-%s %s %s)" % (self.name, self.start_date.day, self.end_date.day, month, self.end_date.year)
+        return name
+    
+    def get_photos(self):
+        photos = GalleryImage.objects.filter(tour=self)
+        return photos
+
+
+class GalleryImage(models.Model):
+    image = models.ImageField(upload_to='gallery-images')
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    tour = models.ForeignKey(Tour, blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.title
+        
+    def get_next(self):
+        photos = []
+        i = 5
+        this_id = self.id
+        while i > 0:
+            try:
+                next_id = this_id + 1
+                photos.append(GalleryImage.objects.get(pk=next_id, tour=self.tour))
+            except:
+                pass
+            i -= 1
+            this_id += 1
+        return photos
+
+    def get_prev(self):
+        photos = []
+        i = 5
+        this_id = self.id
+        while i > 0:
+            try:
+                next_id = this_id - 1
+                photos.append(GalleryImage.objects.get(pk=next_id, tour=self.tour))
+            except:
+                break
+            i -= 1
+            this_id -= 1
+        photos = sorted(photos, key=lambda x: x.id, reverse=False)
+        return photos
+        
+
+class Page(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    content = models.TextField()
+    is_draft = models.BooleanField(default=True)
+    
+    def __unicode__(self):
+        return self.title
